28981dd4791abdd2d94a5e6d38b9f23e0410b27e
jinliye <3211498596@qq.com>
2025-07-22 16:40:02 +0800

    新建员工完成接口测试

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,6 +59,32 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employee) {
+        Employee employee2 = new Employee();
+
+        //对象属性拷贝,从前面的拷贝到后面的
+        BeanUtils.copyProperties(employee, employee2);
+
+        //设置账号状态
+        employee2.setStatus(StatusConstant.ENABLE);
+
+        //设置密码
+        employee2.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        //设置创建时间和修改时间
+        employee2.setCreateTime(LocalDateTime.now());
+        employee2.setUpdateTime(LocalDateTime.now());
+
+        //设置当前记录的创建人id和修改人id
+        // TODO  后续需要改为当前登陆用户的id
+        employee2.setCreateUser(10L);
+        employee2.setUpdateUser(10L);
+
+        employeeMapper.inert(employee2);
     }
 
 }
