f5ebd266b28de8bc86298d4f06e2c45412c69962
JinLiye <3211498596@qq.com>
2025-08-13 23:05:31 +0800

    实现文件上传功能（oss存储）

--- sky-server/src/main/java/com/sky/controller/admin/CommenController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommenController.java
@@ -1,4 +1,45 @@
 package com.sky.controller.admin;
 
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
 public class CommenController {
+
+    private final AliOssUtil aliOssUtil;
+
+    public CommenController(AliOssUtil aliOssUtil) {
+        this.aliOssUtil = aliOssUtil;
+    }
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传，{}",file);
+        try{
+            String fileName = file.getOriginalFilename();
+            String suffix = fileName.substring(fileName.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + suffix;
+
+            String filepath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filepath);
+        } catch (Exception e) {
+            log.error("文件上传失败");
+        }
+
+        return null;
+    }
 }
