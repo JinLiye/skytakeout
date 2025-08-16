7d12ebac2e22f909c29caf59a8ec450401b2ba45
jinliye <3211498596@qq.com>
2025-08-16 16:17:41 +0800

    菜品删除功能

--- sky-server/src/main/java/com/sky/service/DIshService.java
+++ sky-server/src/main/java/com/sky/service/DIshService.java
@@ -5,6 +5,8 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 
+import java.util.List;
+
 public interface DIshService {
     /**
      * 新增菜品和对应的口味数据
@@ -18,4 +20,10 @@ public interface DIshService {
      * @return
      */
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除功能
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
